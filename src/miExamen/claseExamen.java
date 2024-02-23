13ddb409f578d0a7ca86f618d56fe198e25759b7
IvanAlHer <VTP09213@educastur.es>
2024-02-23 19:15:12 +0100

    Añadidos comentarios de documentación en claseExamen.

--- src/miExamen/claseExamen.java
+++ src/miExamen/claseExamen.java
@@ -1,9 +1,21 @@
-
+/** 
+ * 
+ * <head>Entornos de desarrollo UT3</head>
+ * 
+ * <h1>Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de Desarrollo</h1>
+ * 
+ * <link src="https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU"/>
+ */
 package miExamen;
-
+/**
+ * 
+ * @author Ivan
+ * @version 1.0.0
+ * @since 23/02/2024
+ */
 
 public class claseExamen {
     public static void main (String[]args){
-        
+        System.out.println("Estoy haciendo el examen de la UT3");
     }
 }
